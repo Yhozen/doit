0f17d9236e9956d4b355f48547d77a95c0e724d0
Gabriel <gabriel@garox.org>
2018-01-30 01:07:51 -0300

    packaging electron

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const INDEX = 'file://' + path.join(__dirname, 'static/index.html')
 
 app.on('ready', appReady)
 
+let mainWindow 
+
 function appReady () {
   const browserConfig = {
     width: 800,
     height: 600,
     title: APP_NAME
   }
-  let mainWindow = new BrowserWindow(browserConfig)
+  mainWindow = new BrowserWindow(browserConfig)
   mainWindow.loadURL(INDEX)
 
   mainWindow.on('closed', function () {
